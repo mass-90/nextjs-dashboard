f6b0cd1938a54a39f800af3d7679f8a9790de3ea
Mass-90 <mass-90@mail.ru>
2026-04-17 18:56:11 +0400

    fix bags

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,18 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+            `;
+    } catch (error) {
+        // We'll also log the error to the console for now
+        console.error(error);
+        return {
+        message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -42,20 +50,33 @@ export async function updateInvoice(id: string, formData: FormData) {
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    
+
     const amountInCents = amount * 100;
-    
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
-    
+
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to update invoice'); // ❗ исправили
+    }
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+    
+
+
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+export async function updateInvoiceWithId(formData: FormData) {
+    const id = formData.get('id') as string;
+    await updateInvoice(id, formData);
 }
